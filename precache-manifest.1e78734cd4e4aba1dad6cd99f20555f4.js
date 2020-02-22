896ba3a39aec3853de4cf43a25234656f094de5c
dmeats <djmmm@hotmail.com>
2020-02-22 15:57:02 -0500

    Updates

---
+++ precache-manifest.1e78734cd4e4aba1dad6cd99f20555f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1838c54e4fa6bc5390604e4ae652d7",
+    "url": "/react-digital-dash-app/index.html"
+  },
+  {
+    "revision": "0cb68778c32a74b6cb86",
+    "url": "/react-digital-dash-app/static/css/main.af5d4027.chunk.css"
+  },
+  {
+    "revision": "60b1c47fcefaf61c51a3",
+    "url": "/react-digital-dash-app/static/js/2.f3b02067.chunk.js"
+  },
+  {
+    "revision": "0cb68778c32a74b6cb86",
+    "url": "/react-digital-dash-app/static/js/main.0c0a34a4.chunk.js"
+  },
+  {
+    "revision": "11bf0b83fbcc7f53bd3d",
+    "url": "/react-digital-dash-app/static/js/runtime-main.4651ee1b.js"
+  }
+]);
